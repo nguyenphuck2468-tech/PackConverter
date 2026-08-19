eab13eadc9835042b184dfecd3d071d606a9ca7f
nguyenphuck2468-tech <nguyenphuck2468@gmail.com>
2026-08-20 06:46:39 +0700

    Remove unused extractor import before CI

--- converter/src/main/java/org/geysermc/pack/converter/util/ModJarExtractor.java
+++ converter/src/main/java/org/geysermc/pack/converter/util/ModJarExtractor.java
@@ -27,7 +27,6 @@ import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
